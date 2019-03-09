293fcd937a19c99c792dd64c40f8cffb539a7fcb
Couser94 <bernardo94rz@gmail.com>
2019-03-09 13:40:42 -0600

    Inicio validar campos PHP

---
+++ validate_form_registro.php
@@ -0,0 +1,11 @@
+<?php
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$celular = $_POST['celular'];
+	$contrasena = $_POST['contrasena'];
+	$confirmacion_contrasena = $_POST['confirmacion_contrasena'];
+
+	/*VALIDAR TEXTO Y ENVIAR RESPUESTA*/
+	/*if todo salio bien = true, else = false*/
+	echo("true");
+?>
